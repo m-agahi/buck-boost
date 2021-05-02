0e3b6bf35973dad38f65946e3730b792771ff68f
Max Agahi <m.agahi@outlook.com>
2021-05-02 13:08:02 +0200

    Tidy up and new working boost-7.X

---
+++ boost-7/boost-7-master.X/build/default/production/mcc_generated_files/slave1.o.d
@@ -0,0 +1,10 @@
+build/default/production/mcc_generated_files/slave1.o: \
+ mcc_generated_files/slave1.c mcc_generated_files/slave1.h \
+ mcc_generated_files/slave_typedef.h \
+ /home/max/University/DigitalControl/Curosity/git/buck-boost/boost-7/slave.X/dist/default/production/slave.h
+
+mcc_generated_files/slave1.h:
+
+mcc_generated_files/slave_typedef.h:
+
+/home/max/University/DigitalControl/Curosity/git/buck-boost/boost-7/slave.X/dist/default/production/slave.h:
